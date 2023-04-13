f3bd9f8bf7840be7412d2812698bbf2e7c958f7f
Basir Rahimi <basiralirahimi@gmail.com>
2023-04-13 11:50:18 +0100

    Navigate slides with scroll & update footer year

--- gulpfile.js
+++ gulpfile.js
@@ -15,79 +15,80 @@ var eslint = require('gulp-eslint');
 
 sass.compiler = require('node-sass');
 
+function reload(cb) {
+    browserSync.reload();
+    cb();
+}
+
 function browser_sync() {
-  browserSync.init({
-    server: {
-      baseDir: "./dist/"
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "./dist/"
+        }
+    });
 };
 
-function reload(cb) {
-  browserSync.reload();
-  cb();
+function scss() {
+    return gulp.src('./source/scss/components/**/*.scss', { base: './source/scss/' })
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./source/css'));
 }
 
-function css(cb) {
-  gulp.src('./source/scss/app.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 versions'],
-      casade: false
-    }))
-    .pipe(gulp.dest('./source/css'))
-    .pipe(cssmin())
-    .pipe(concat('build.css'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/css'));
-
-  gulp.src('./source/scss/components/**/*.scss', { base: './source/scss/' })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./source/css'));
-
-  cb();
+function css() {
+    return gulp.src('./source/scss/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            casade: false
+        }))
+        .pipe(gulp.dest('./source/css'))
+        .pipe(cssmin())
+        .pipe(concat('build.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream({ match: '**/*.css' }));
 }
 
-function js(cb) {
-  gulp.src(['./source/js/**/*.js', '!./source/js/{bootstrap,bootstrap/**}'])
-    .pipe(sourcemaps.init())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    // .pipe(eslint.failAfterError())
-    .pipe(concat('build.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/js'));
-  cb();
+function js() {
+    return gulp.src(['./source/js/**/*.js', '!./source/js/{bootstrap,bootstrap/**}'])
+        .pipe(sourcemaps.init())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        // .pipe(eslint.failAfterError())
+        .pipe(concat('build.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.stream());
 };
 
 // Minify images and cache them
 function images(cb) {
-  gulp.src('./source/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // .pipe(cache(imagemin({
-    //     interlaced: true
-    // })))
-    .pipe(gulp.dest('dist/images'));
-  cb();
+    return gulp.src('./source/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        // .pipe(cache(imagemin({
+        //     interlaced: true
+        // })))
+        .pipe(gulp.dest('dist/images'));
+    cb();
 };
 
 function watch() {
-  gulp.watch('./source/scss/**/*.scss', gulp.series(css, reload));
-  gulp.watch('./source/js/**/*.js', gulp.series(js, reload));
-  gulp.watch('./source/images/**/*.+(png|jpg|jpeg|gif|svg)', gulp.series(images, reload));
-  gulp.watch('./dist/**/*.html', reload);
+    gulp.watch('./source/scss/**/*.scss', gulp.series(scss, css));
+    gulp.watch('./source/js/**/*.js', gulp.series(js));
+    gulp.watch('./source/images/**/*.+(png|jpg|jpeg|gif|svg)', gulp.series(images, reload));
+    gulp.watch('./dist/**/*.html', reload);
 };
 
 exports.default = gulp.series(
-  gulp.parallel(css, js, images),
-  gulp.parallel(watch, browser_sync)
+    gulp.parallel(gulp.series(scss, css), js, images),
+    gulp.parallel(watch, browser_sync)
 );
 exports.js = gulp.task(js)
--- source/js/app.js
+++ source/js/app.js
@@ -1,14 +1,15 @@
-var currentActiveScreen     = 0;
-var background              = null;
-var screenPositions         = [];
-var navItems                = null;
-var progressBar             = null;
+var currentActiveScreen = 0;
+var background = null;
+var screenPositions = [];
+var navItems = null;
+var progressBar = null;
 
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DomContentLoaded');
     var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
-    if(window.innerHeight > window.innerWidth || orientation === 'portrait-secondary' || orientation === 'portrait-primary'){
+    if (window.innerHeight > window.innerWidth || orientation === 'portrait-secondary' || orientation === 'portrait-primary') {
         document.getElementById('landscapeNotice').style.display = 'block';
-        document.addEventListener('orientationchange', ()=>{
+        document.addEventListener('orientationchange', () => {
             init();
         })
     } else {
@@ -18,16 +19,35 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 function init() {
     document.body.classList.add('initialised');
-    background          = document.getElementById('background');
-    navItems            = document.getElementsByClassName('nav-item');
-    progressBar         = document.getElementById('progress');
-    
+
+    var year = document.getElementById('copyrightYear');
+    year.innerHTML = new Date().getFullYear();
+
+    background = document.getElementById('background');
+    navItems = document.getElementsByClassName('nav-item');
+    progressBar = document.getElementById('progress');
+
     var sections = document.getElementsByClassName('art-section');
     for (let i = 0; i < sections.length; i++) {
         screenPositions.push({
             x: sections[i].offsetLeft - (sections[i].offsetLeft * 2),
             y: sections[i].offsetTop - (sections[i].offsetTop * 2)
         })
+    }
+
+    var artboard = document.getElementById('artboard');
+    artboard.addEventListener('wheel', (event) => {
+        event.preventDefault();
+
+        navigateViaScroll(event.deltaY);
+    });
+}
+
+function navigateViaScroll(deltaY) {
+    if (deltaY > 0 && currentActiveScreen < 11) {
+        goToSection(currentActiveScreen + 1);
+    } else if (deltaY < 0 && currentActiveScreen > 0) {
+        goToSection(currentActiveScreen - 1);
     }
 }
 
@@ -50,17 +70,17 @@ function updateElements() {
     }
 
     //add active class to the correct nav item
-    if(currentActiveScreen < 11) {
-        var activeItem = navItems[currentActiveScreen-1];
-        if(activeItem) {
+    if (currentActiveScreen < 11) {
+        var activeItem = navItems[currentActiveScreen - 1];
+        if (activeItem) {
             activeItem.classList.add('active');
             prevButton.classList.remove('disabled');
         } else {
             prevButton.classList.add('disabled');
         }
     }
-    if(currentActiveScreen < 11) {
-        progressBar.style.width = `${Math.abs(currentActiveScreen-10)*10}%`;
+    if (currentActiveScreen < 11) {
+        progressBar.style.width = `${Math.abs(currentActiveScreen - 10) * 10}%`;
         nextButton.classList.remove('disabled');
     } else {
         nextButton.classList.add('disabled');
@@ -68,5 +88,5 @@ function updateElements() {
 }
 
 function progressBarTo(section) {
-    progressBar.style.width = `${Math.abs(section-10)*10}%`;
+    progressBar.style.width = `${Math.abs(section - 10) * 10}%`;
 }
